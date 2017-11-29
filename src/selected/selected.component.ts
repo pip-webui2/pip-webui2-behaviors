bdd572e6bf1714eafa8480ff610c998a54d6e7f2
Алексей Двойкин <94dvoikin@gmail.com>
2017-11-29 20:33:36 +0300

    [Selected] update component

--- src/selected/selected.component.ts
+++ src/selected/selected.component.ts
@@ -1,13 +1,5 @@
 import { Component, Input, Output, OnInit, AfterViewInit, EventEmitter, Renderer, ElementRef, HostListener } from '@angular/core';
-
-class KEY_CODE {
-    public static DOWN_ARROW = 40;
-    public static UP_ARROW = 38;
-    public static LEFT_ARROW = 37;
-    public static RIGHT_ARROW = 39;
-    public static ENTER = 13;
-    public static SPACE = 32;
-}
+import { KEY_CODE } from '../shared/key-code.model';
 
 @Component({
     selector: 'pip-selected',
